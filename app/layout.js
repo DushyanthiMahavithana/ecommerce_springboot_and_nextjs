a3a5b78d5738bdd20bc6951dda2412865abd6282
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-11-11 22:44:46 +0530

    Created the home page banner

--- app/layout.js
+++ app/layout.js
@@ -14,13 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className='max-w-8xl mx-auto'>
+        <div className='max-w-8xl mx-auto'>
           <Navbar />
-          <Footer />
-      </div>
-      <div>
           {children}
-     </div>
+          <Footer />
+        </div>
       </body>
     </html>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,21 @@
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import HomeBanner from '../images/homebanner.jpg';
+import Link from 'next/link';
 
 export default function Home() {
-  
-  
+    return(
+    <div className='relative max-w-full overflow-x-hidden'>
+        <Image src={HomeBanner} alt='home banner' width={1600} ></Image>
+        <div className='absolute top-1/3 left-1/3 transform-translate-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-white'>
+            <p className='mt-20 text-4xl font-medium'>Welcome to the</p>
+            <h1 className='mb-10 text-6xl italic font-bold'>Animal Kingdom</h1>
+            <p className='text-lg max-w-3xl mx-auto mb-12'>We're delighted to present a revolutionary pet food experience designed to elevate your furry friend's well-being. Our commitment to excellence is embedded in every kibble, promising a feast of flavors and nourishment that goes beyond the bowl.</p>
+            <Link href={"/#"}>  
+            <button className='bg-black hover:bg-gray-500 py-4 px-10 rounded-xl'>SHOP NOW</button>
+            </Link>
+        </div>
+    </div>
+
+    );
 }
