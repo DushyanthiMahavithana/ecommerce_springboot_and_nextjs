3a8a353000b972767b017a41276b12d6fe19b11b
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-11-25 10:05:53 +0530

    added state for search term

--- app/searchbar/page.jsx
+++ app/searchbar/page.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import {FiHeart} from "react-icons/fi";
+import data from '../db/data';
 
 export default function searchbar() {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <div>
     <div className='flex items-center justify-between p-4 bg-slate-200 text-black ml-40 mb-10'>
       <input 
       className='px-2 py-1 mr-2 border rounded focus:outline' 
       type='text'
-      placeholder='Search'>
+      placeholder='Search'
+      value={searchTerm}
+      onChange={(e)=>setSearchTerm(e.target.value)}>
+      
       </input>
     
       <div className='flex'>
