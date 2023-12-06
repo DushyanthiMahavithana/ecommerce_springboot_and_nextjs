3bb10bbfb93ceb27866498a61cc9e42b02ae6e2a
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2023-12-06 11:17:43 +0530

    search bar

--- app/searchbar/page.jsx
+++ app/searchbar/page.jsx
@@ -1,66 +1,49 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FiHeart } from "react-icons/fi";
 import data from '../db/data';
 import product from '../products/page';
+import { useRouter } from 'next/navigation';
 
 export default function Searchbar() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [suggestions, setSuggestions] = useState([]); 
+  const [searchQuery, setSearchQuery] = useState('');
+  const router = useRouter();
+  
+  
+  const onSearch = (event) => {
+    event.preventDefault();
 
-  const filterProducts = data.filter((product) => 
-    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
-  const handleInputChange = (e) => {
-    const value = e.target.value;
-    setSearchTerm(value);
+    const encodedSearchQuery = encodeURI(searchQuery);
+    router.push(`/search?q=${encodedSearchQuery}`);
 
-    // Update suggestions based on the filtered products
-    setSuggestions(
-      filterProducts.map((product) => product.name) // Fixed variable name here
-    );
-  };
+    console.log("current query", encodedSearchQuery);
+  
 
-  const handleSuggestionClick = (suggestion) => {
-    // Set the selected suggestion as the search term
-    setSearchTerm(suggestion);
-    // Clear suggestions
-    setSuggestions([]);
   };
 
   return (
     <div>
       <div className='flex items-center justify-between p-4 bg-slate-200 text-black ml-40 mb-10'>
+        <form onSubmit={onSearch}>
         <input
-          className='px-2 py-1 mr-2 border rounded focus:outline'
-          type='text'
-          placeholder='Search'
-          value={searchTerm}
-          onChange={handleInputChange}
+        value={searchQuery}
+        onChange={(event) => searchQuery(event.target.value)}
+        className='px-2 py-1 mr-2 border rounded focus:outline'
+        type='text'
+        placeholder='Search'
         />
 
-        {suggestions.length > 0 && (
-          <ul className="absolute left-0 mt-2 w-full bg-white border rounded">
-            {suggestions.map((suggestion, index) => (
-              <li
-                key={index}
-                className="p-2 cursor-pointer hover:bg-gray-200"
-                onClick={() => handleSuggestionClick(suggestion)}
-              >
-                {suggestion}
-              </li>
-            ))}
-          </ul>
-        )}
-
+       
         <div className='flex'>
           <Link className="hover:text-gray-400 mr-8" href={"/#"}><FiHeart size={25} /></Link>
           <Link className="hover:text-gray-400 mr-8" href={"/#"}><AiOutlineShoppingCart size={25} /></Link>
         </div>
+        </form>
       </div>
     </div>
   );
-}
+};
+
